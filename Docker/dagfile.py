970fcea4b89f470388126075b09293278e8e81a0
entrancestone <45136776+entrancestone@users.noreply.github.com>
2020-06-08 17:46:19 -0400

    Update dagfile.py

--- Docker/dagfile.py
+++ Docker/dagfile.py
@@ -25,7 +25,7 @@ default_args = {
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
-    'schedule_interval': '@daily',
+    'schedule_interval': '@hourly',
     # 'queue': 'bash_queue',
     # 'pool': 'backfill',
     # 'priority_weight': 10,
